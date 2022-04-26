2f30b9380123b9027f02b04c390a599f7b935681
yongveloper <aaa9015@naver.com>
2022-04-26 12:23:08 +0900

    [기능] 로그인, 회원가입 구현

---
+++ server/src/controller/auth.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import {} from 'express-async-errors';
+import * as userRepository from '../data/auth';
+
+const jwtSecetKey = 'wKIz4@gjoAPakH!H%PCw!v0*W&RtHdYa';
+const jwtExpiresDays = '2d';
+const bcryptSaltRounds = 12;
+
+export async function signup(req: Request, res: Response) {
+  const { username, email, password } = req.body;
+  const found = await userRepository.findByEmail(email);
+  if (found) {
+    return res.status(409).json({ message: `${email} already exists` });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, bcryptSaltRounds);
+  const userId = await userRepository.createUser({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  const token = createJwtToken(userId);
+
+  res.status(201).json({ token, email });
+}
+
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+  const user = await userRepository.findByEmail(email);
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid user or passowrd' });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (isValidPassword) {
+    return res.status(401).json({ message: 'Invalid user or passowrd' });
+  }
+
+  if (user.id) {
+    const token = createJwtToken(user.id);
+    res.status(201).json({ token, email });
+  }
+}
+
+function createJwtToken(id: string) {
+  return jwt.sign({ id }, jwtSecetKey, { expiresIn: jwtExpiresDays });
+}
