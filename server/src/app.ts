443d78a952516fd2fea52e8843d932fbad82b9af
yongveloper <aaa9015@naver.com>
2022-04-26 10:49:14 +0900

    [구성] cors 패키지 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 import diariesRouter from './router/diaries';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny'));
 
