b4fd1833996ee94f02f0d496ec35352cb1a966f4
yongveloper <aaa9015@naver.com>
2022-04-27 16:43:51 +0900

    [기능] Add Validate in Auth router

--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import {} from 'express-async-errors';
+import { body } from 'express-validator';
 import * as authController from '../controller/auth';
 import { isAuth } from '../middleware/auth';
+import { validate } from '../middleware/validator';
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+const validateCredential = [
+  body('email').isEmail().normalizeEmail().withMessage('invalid email'),
+  body('password')
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage('password should be at least 6 characters'),
+  validate,
+];
 
-router.post('/login', authController.login);
+const validateSignup = [
+  ...validateCredential,
+  body('username').notEmpty().withMessage('name is missing'),
+];
+
+router.post('/signup', validateSignup, authController.signup);
+
+router.post('/login', validateCredential, authController.login);
 
 router.get('/me', isAuth, authController.me);
 
