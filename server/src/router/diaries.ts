ca62e1f20eeda6c7178e6c538c66176ba70399f5
Yongveloper <aaa9015@naver.com>
2022-04-27 23:36:49 +0900

    [기능] Diary Router 구현 (더미)

--- server/src/router/diaries.ts
+++ server/src/router/diaries.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-import * as diariesRepository from '../controller/diaries';
+import * as diaryController from '../controller/diaries';
 
 const router = express.Router();
 
-router.get('/', diariesRepository.getDiaries);
+router.get('/', diaryController.getDiaries);
+
+router.post('/', diaryController.createDiary);
+
+router.put('/:id', diaryController.updateDiary);
+
+router.delete('/:id', diaryController.deleteDairy);
 
 export default router;
