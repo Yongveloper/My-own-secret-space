78e405af6f24901d219e329524520a8cb102fa0f
yongveloper <aaa9015@naver.com>
2022-04-29 15:26:36 +0900

    [기능] Add middleware in diaries router

--- server/src/router/diaries.ts
+++ server/src/router/diaries.ts
@@ -1,14 +1,29 @@
 import express from 'express';
+import { body } from 'express-validator';
 import * as diaryController from '../controller/diaries';
+import { isAuth } from '../middleware/auth';
+import { validate } from '../middleware/validator';
 
 const router = express.Router();
 
-router.get('/', diaryController.getDiaries);
+const validateDiary = [
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('title should be at least 3 characters'),
+  body('text')
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage('text should be at least 10 characters'),
+  validate,
+];
 
-router.post('/', diaryController.createDiary);
+router.get('/', isAuth, diaryController.getDiaries);
 
-router.put('/:id', diaryController.updateDiary);
+router.post('/', isAuth, validateDiary, diaryController.createDiary);
 
-router.delete('/:id', diaryController.deleteDairy);
+router.put('/:id', isAuth, validateDiary, diaryController.updateDiary);
+
+router.delete('/:id', isAuth, diaryController.deleteDairy);
 
 export default router;
