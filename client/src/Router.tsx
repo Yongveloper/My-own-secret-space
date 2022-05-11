4ffd026e8233d5a3d2e6eef43454675be6f8ef2b
Yongveloper <aaa9015@naver.com>
2022-05-11 23:23:25 +0900

    [변경] 폴더 분류 pages

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function Router() {
   return (
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import { RiGitRepositoryPrivateFill } from 'react-icons/ri';
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center w-fll h-screen ">
       <h1 className="flex items-center justify-center text-3xl font-bold">
         <RiGitRepositoryPrivateFill className="text-amber-500" />
         나만의 비밀 공간
       </h1>
-      <h2>나의 하루를 기억하고 기억해보세요</h2>
-      <button className="bg-purple-600 hover:bg-purple-800 font-bold py-2 px-12 rounded">
+      <h2>나의 하루를 기억하고 기록 해보세요</h2>
+      <button className="bg-violet-700 hover:bg-violet-800 w-full py-2 px-12 rounded">
         로그인
       </button>
-      <button className="bg-purple-600 hover:bg-purple-800 font-bold py-2 px-12 rounded">
+      <button className="bg-fuchsia-600 hover:bg-fuchsia-800 w-full py-2 px-12 rounded">
         가입하기
       </button>
     </div>
