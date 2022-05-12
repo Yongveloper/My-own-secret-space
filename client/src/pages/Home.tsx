340f3d36dcde6b6cd394ecd14d5eca8eec05fdfb
yongveloper <aaa9015@naver.com>
2022-05-12 12:10:28 +0900

    [스타일] Home Markup and Styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import { RiGitRepositoryPrivateFill } from 'react-icons/ri';
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-fll h-screen ">
-      <h1 className="flex items-center justify-center text-3xl font-bold">
-        <RiGitRepositoryPrivateFill className="text-amber-500" />
-        나만의 비밀 공간
-      </h1>
-      <h2>나의 하루를 기억하고 기록 해보세요</h2>
-      <button className="bg-violet-700 hover:bg-violet-800 w-full py-2 px-12 rounded">
-        로그인
-      </button>
-      <button className="bg-fuchsia-600 hover:bg-fuchsia-800 w-full py-2 px-12 rounded">
-        가입하기
-      </button>
-    </div>
+    <main className="content-wrap">
+      <div className="text-center">
+        <h1 className="flex items-center justify-center text-4xl font-bold mb-3">
+          <RiGitRepositoryPrivateFill className="text-amber-500" />
+          나만의 비밀 공간
+        </h1>
+        <h2 className="text-xl">나의 하루를 기억하고 기록 해보세요</h2>
+      </div>
+      <div className="mt-10">
+        <button className="button bg-violet-700 active:bg-violet-800 mb-4">
+          로그인
+        </button>
+        <button className="button bg-fuchsia-600 active:bg-fuchsia-800 ">
+          가입하기
+        </button>
+      </div>
+    </main>
   );
 }
 
