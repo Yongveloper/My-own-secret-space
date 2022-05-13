4853cb151ea40f7be630774e708afed18a992900
yongveloper <aaa9015@naver.com>
2022-05-13 15:36:27 +0900

    [테스트] 로그인 페이지 테스트 코드 성공 renders elements

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import LoginBtn from '../components/common/Buttons/LoginBtn';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="content-wrap">
+      <h1>로그인</h1>
+      <form>
+        <input type="email" placeholder="이메일" />
+        <input type="password" placeholder="비밀번호" />
+        <LoginBtn />
+      </form>
+      <div>
+        <span>아직 계정이 없으신가요? 계정 만들기</span>
+      </div>
+      <div>
+        <span>또는</span>
+      </div>
+      <button className="button">Kakao로 계속하기</button>
+      <button className="button">Facebook으로 계속하기</button>
+    </div>
+  );
 }
 
 export default Login;
