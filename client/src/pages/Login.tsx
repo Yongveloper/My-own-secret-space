b5167e2e830bc429d68e228c2e82cdbc6302ca23
yongveloper <aaa9015@naver.com>
2022-05-13 15:50:57 +0900

    [스타일] 중간저장 input style

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,10 +3,18 @@ import LoginBtn from '../components/common/Buttons/LoginBtn';
 function Login() {
   return (
     <div className="content-wrap">
-      <h1>로그인</h1>
+      <h1 className="text-4xl font-bold mb-3">로그인</h1>
       <form>
-        <input type="email" placeholder="이메일" />
-        <input type="password" placeholder="비밀번호" />
+        <input
+          type="email"
+          placeholder="이메일"
+          className="w-full px-2 py-1 border-solid border-2 rounded text-neutral-900 mb-2"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="w-full px-2 py-1 border-solid border-2 rounded text-neutral-900 mb-2"
+        />
         <LoginBtn />
       </form>
       <div>
@@ -15,8 +23,10 @@ function Login() {
       <div>
         <span>또는</span>
       </div>
-      <button className="button">Kakao로 계속하기</button>
-      <button className="button">Facebook으로 계속하기</button>
+      <button className="button bg-amber-500 text-neutral-900">
+        Kakao로 계속하기
+      </button>
+      <button className="button bg-blue-700">Facebook으로 계속하기</button>
     </div>
   );
 }
