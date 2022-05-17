684d56eab09b47924b11ba3fd159edd42190ef7b
yongveloper <aaa9015@naver.com>
2022-05-17 10:50:49 +0900

    [개선] Login Form 컴포넌트 import

--- client/src/pages/tests/Login.test.tsx
+++ client/src/pages/tests/Login.test.tsx
@@ -4,17 +4,14 @@ import Login from '../Login';
 describe('<Login />', () => {
   describe('default field must be rendered', () => {
     it('has title, inputs, two span and three buttons.', () => {
+      // Todo: 계정만들기 링크태그 테스트 포함 및 개선
       render(<Login />);
       const title = screen.getByRole('heading', { level: 1 });
-      const emailInput = screen.getByPlaceholderText('이메일');
-      const pwInput = screen.getByPlaceholderText('비밀번호');
-      const span1 = screen.getByText('아직 계정이 없으신가요? 계정 만들기');
+      const span1 = screen.getByText('아직 계정이 없으신가요?');
       const span2 = screen.getByText('또는');
       const buttons = screen.getAllByRole('button');
 
       expect(title).toBeInTheDocument();
-      expect(emailInput).toBeInTheDocument();
-      expect(pwInput).toBeInTheDocument();
       expect(span1).toBeInTheDocument();
       expect(span2).toBeInTheDocument();
       expect(buttons.length).toBe(3);
