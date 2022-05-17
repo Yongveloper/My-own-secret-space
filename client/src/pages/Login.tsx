47d332d2c9c8ff3e38e729d862eb0dbf14aad342
yongveloper <aaa9015@naver.com>
2022-05-17 16:15:36 +0900

    [기능] Link 추가 로그인 페이지에서 가입하기 페이지로 이동하는 링크 태그로 변경

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import GoogleBtn from '../components/common/Buttons/GoogleBtn';
 import KakaoBtn from '../components/common/Buttons/KakaoBtn';
 import LoginForm from '../components/Login/LoginForm';
@@ -9,7 +10,10 @@ function Login() {
       <LoginForm />
       <div className="flex flex-col justify-center items-center m-3">
         <span className="mb-2 text-gray-600">
-          아직 계정이 없으신가요? <span className="underline">계정 만들기</span>
+          아직 계정이 없으신가요?{' '}
+          <Link to="/signup" className="underline">
+            계정 만들기
+          </Link>
         </span>
         <span>또는</span>
       </div>
--- client/src/pages/tests/Login.test.tsx
+++ client/src/pages/tests/Login.test.tsx
@@ -1,19 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
 import Login from '../Login';
 
 describe('<Login />', () => {
   describe('default field must be rendered', () => {
     it('has title, inputs, two span and three buttons.', () => {
       // Todo: 계정만들기 링크태그 테스트 포함 및 개선
-      render(<Login />);
+      const history = createMemoryHistory({ initialEntries: ['/login'] });
+      render(
+        <Router location={history.location} navigator={history}>
+          <Login />
+        </Router>
+      );
       const title = screen.getByRole('heading', { level: 1 });
       const span1 = screen.getByText('아직 계정이 없으신가요?');
       const span2 = screen.getByText('또는');
+      const signupLink = screen.getByText('계정 만들기');
       const buttons = screen.getAllByRole('button');
 
       expect(title).toBeInTheDocument();
       expect(span1).toBeInTheDocument();
       expect(span2).toBeInTheDocument();
+      expect(signupLink).toBeInTheDocument();
       expect(buttons.length).toBe(3);
     });
   });
