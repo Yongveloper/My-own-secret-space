baae2548178c74a25402f1496b7661da4c0b5698
Yongveloper <aaa9015@naver.com>
2022-05-17 21:07:06 +0900

    [스타일] 카카오 버튼 색상 변경 짙은 노란색에서 연한 노란색으로 색 변경함(카카오톡 색과 비슷하게)

--- client/src/components/common/Buttons/KakaoBtn.tsx
+++ client/src/components/common/Buttons/KakaoBtn.tsx
@@ -2,7 +2,7 @@ import { RiKakaoTalkFill } from 'react-icons/ri';
 
 function KakaoBtn() {
   return (
-    <button className="button bg-amber-500 text-neutral-900">
+    <button className="button bg-yellow-400 text-neutral-900">
       <RiKakaoTalkFill className="mr-1 text-lg" />
       Kakao로 계속하기
     </button>
