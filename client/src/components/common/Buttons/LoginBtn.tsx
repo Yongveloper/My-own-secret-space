2b19c0172e7bc2bf6aa7c78ab7ab165e6173f2af
yongveloper <aaa9015@naver.com>
2022-05-17 10:34:46 +0900

    [스타일] 로그인 라우트 스타일링

--- client/src/components/common/Buttons/LoginBtn.tsx
+++ client/src/components/common/Buttons/LoginBtn.tsx
@@ -1,6 +1,6 @@
 function LoginBtn() {
   return (
-    <button className="button bg-violet-700 active:bg-violet-800 mb-4">
+    <button className="button bg-violet-700 active:bg-violet-800">
       로그인
     </button>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,32 +1,30 @@
+import { BsFacebook } from 'react-icons/bs';
+import { RiKakaoTalkFill } from 'react-icons/ri';
 import LoginBtn from '../components/common/Buttons/LoginBtn';
 
 function Login() {
   return (
     <div className="content-wrap">
-      <h1 className="text-4xl font-bold mb-3">로그인</h1>
+      <h1 className="text-4xl font-bold mb-8">로그인</h1>
       <form>
-        <input
-          type="email"
-          placeholder="이메일"
-          className="w-full px-2 py-1 border-solid border-2 rounded text-neutral-900 mb-2"
-        />
-        <input
-          type="password"
-          placeholder="비밀번호"
-          className="w-full px-2 py-1 border-solid border-2 rounded text-neutral-900 mb-2"
-        />
+        <input type="email" placeholder="이메일" className="login-input" />
+        <input type="password" placeholder="비밀번호" className="login-input" />
         <LoginBtn />
       </form>
-      <div>
-        <span>아직 계정이 없으신가요? 계정 만들기</span>
-      </div>
-      <div>
+      <div className="flex flex-col justify-center items-center m-3">
+        <span className="mb-2 text-gray-600">
+          아직 계정이 없으신가요? <span className="underline">계정 만들기</span>
+        </span>
         <span>또는</span>
       </div>
       <button className="button bg-amber-500 text-neutral-900">
+        <RiKakaoTalkFill className="mr-1 text-lg" />
         Kakao로 계속하기
       </button>
-      <button className="button bg-blue-700">Facebook으로 계속하기</button>
+      <button className="button bg-blue-700">
+        <BsFacebook className="mr-1" />
+        Facebook으로 계속하기
+      </button>
     </div>
   );
 }
