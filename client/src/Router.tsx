f8fb578a1468831b5baeb342042d3b7774296a66
Yongveloper <aaa9015@naver.com>
2022-05-17 23:42:18 +0900

    [개선] 사용하지 않는 import 제거

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import MyDiaries from './pages/MyDiaries';
 
 function Router() {
   return (
@@ -9,7 +10,8 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/myDiaries" element={<MyDiaries />} />
       </Routes>
     </BrowserRouter>
   );
