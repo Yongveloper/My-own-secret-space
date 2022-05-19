70808b0889edded29550c55f51af8b79d8991210
yongveloper <aaa9015@naver.com>
2022-05-19 16:05:17 +0900

    [스타일] 일기 상세 페이지 스타일링

--- client/src/components/MyDiaries/MyDiaryItem.tsx
+++ client/src/components/MyDiaries/MyDiaryItem.tsx
@@ -19,7 +19,7 @@ function MyDiaryItem({
   return (
     <Link to={`/mydiaries/${id}`}>
       <li className="flex flex-col justify-center bg-white h-2/4 mb-3 p-3 border-2 border-slate-400">
-        <h1 className="font-bold text-xl text-gray-900">{title}</h1>
+        <h1 className="font-bold text-xl text-gray-900 text-center">{title}</h1>
         <span role="img">{mood}</span>
         <div className="text-sm text-gray-500" data-testid="Date">
           {dateString}
--- client/src/pages/DiaryDetail.tsx
+++ client/src/pages/DiaryDetail.tsx
@@ -1,5 +1,34 @@
+import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs';
+
 function DiaryDetail() {
-  return <div>DiaryDetail</div>;
+  return (
+    <div className="max-w-sm flex flex-col items-center justify-center w-screen h-auto px-8 py-8">
+      <span role="img" data-testid="Mood">
+        😀
+      </span>
+      <h3 className="text-base text-gray-500" data-testid="Date">
+        2022년 10월 22일
+      </h3>
+      <h1 className="text-4xl font-bold text-center">의미 있는 날</h1>
+      <div className="w-full flex justify-end mt-3 mb-3">
+        <button className="flex items-center mr-3 text-gray-400">
+          <BsFillPencilFill /> 수정
+        </button>
+        <button className="flex items-center text-red-400">
+          <BsFillTrashFill /> 삭제
+        </button>
+      </div>
+      <img
+        src="https://images.unsplash.com/photo-1652703747774-558a10faacc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+        alt="thumbnail"
+        data-testid="Thumbnail"
+      />
+      <div className="mt-3" data-testid="Text">
+        오늘은 의미있는 날이었다 그런데오늘은 의미있는 날이었다 그런데오늘은
+        의미있는 날이었다 그런데
+      </div>
+    </div>
+  );
 }
 
 export default DiaryDetail;
